58855149ca43c68e5d5735a75d1d25ef43965267
Eduard <eddjimlab@gmail.com>
2019-08-20 18:52:12 +0300

    Updates

---
+++ precache-manifest.20f0b5703a65db30d057f42dd14ccf82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e9f30ff3a95d91735c6e8efb30045c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "624de9c3a84742ea735a",
+    "url": "/static/css/main.fcb48c17.chunk.css"
+  },
+  {
+    "revision": "5eef02cacd4b0fdaa67f",
+    "url": "/static/js/2.31cdb932.chunk.js"
+  },
+  {
+    "revision": "624de9c3a84742ea735a",
+    "url": "/static/js/main.c62b5288.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
